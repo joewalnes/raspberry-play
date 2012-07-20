01fba57aa49bb0e44a4069f7d18fecb6ed50be89
Brian Adams <badams@drw.com>
2012-07-20 10:19:34 -0500

    python 3.2 compatibility

--- web-slider/main.py
+++ web-slider/main.py
@@ -6,12 +6,12 @@ def main():
   webserver = WebServer(port=8888, debug=True)
   webserver.websocket('/slider-value', SharedValue(20, on_change=slider_updated))
   webserver.static_files('/', './static')
-  print 'Listening on %s' % webserver.url
+  print('Listening on %s' % webserver.url)
   webserver.run()
 
 def slider_updated(value, connection):
   # TODO: Communicate with LED bar graph
-  print 'Slider value: ', value
+  print('Slider value: ', value)
 
 if __name__ == '__main__':
   main()
